a36e7ef6e3f1336207029fb73f2a8df3bc298171
George Kapetanakis <george.kapetanakis.gk@gmail.com>
2026-02-25 10:50:12 -0500

    Fix functionality when V2M_HOME or V2M_REPO contain whitespace

--- v2m/main.sh
+++ v2m/main.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # FIX: async `preview` command
-REPO_FILES="find $V2M_REPO -type f"
-eval $REPO_FILES | fzf \
+REPO_FILES="find \"$V2M_REPO\" -type f"
+eval "$REPO_FILES" | fzf \
 --border=rounded \
 --margin=2.5% \
 --multi \
---preview="$V2M_HOME/v2m/config_detail.py {}" \
+--preview="\"$V2M_HOME\"/v2m/config_detail.py {}" \
 --preview-label="[ Server Detail ]" \
 --preview-window="40%,border-rounded,wrap" \
 \
@@ -14,15 +14,15 @@ eval $REPO_FILES | fzf \
 --bind="ctrl-h:execute(rm -ri {+})" \
 --bind="left:execute(rm -ri {+})" \
 \
---bind="+:execute(touch $V2M_REPO/{q})" \
+--bind="+:execute(touch \"$V2M_REPO\"/{q})" \
 \
---bind="del:+reload(eval $REPO_FILES)" \
---bind="ctrl-h:+reload(eval $REPO_FILES)" \
---bind="left:+reload(eval $REPO_FILES)" \
---bind="+:+reload(eval $REPO_FILES)" \
---bind="ctrl-r:reload(eval $REPO_FILES)" \
+--bind="del:+reload(eval \"$REPO_FILES\")" \
+--bind="ctrl-h:+reload(eval \"$REPO_FILES\")" \
+--bind="left:+reload(eval \"$REPO_FILES\")" \
+--bind="+:+reload(eval \"$REPO_FILES\")" \
+--bind="ctrl-r:reload(eval \"$REPO_FILES\")" \
 \
---bind="enter:execute(bash $V2M_HOME/v2m/set_config.sh {})" \
+--bind="enter:execute(bash \"$V2M_HOME\"/v2m/set_config.sh {})" \
 --bind="right:execute(vim {+})" \
 --bind="ctrl-l:execute(vim {+})" \
 \
